646e7a3778a09df7469889beaa6c7f4b5a8be5f9
Vasiliy-Velikiy <test@mail.ru>
2023-10-16 10:31:13 +0300

    refactoring

--- CJLIBTest/src/main/java/ru/moskalev/CJLibTestProjectApplication.java
+++ CJLIBTest/src/main/java/ru/moskalev/CJLibTestProjectApplication.java
@@ -1,6 +1,5 @@
 package ru.moskalev;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
